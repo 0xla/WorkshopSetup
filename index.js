a90ee984e4dac2a548984e95141e610a796a47d7
UBC Student <lakshy01@students.cs.ubc.ca>
2022-04-01 19:14:53 -0700

    initial set up

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+const q = require("./queries.js");
+require("dotenv").config();
+
+
+app.post(`/purchase/:api_key`, q.brockChain.purchase)
+// app.get(`trackTransations/:api_key`, q.brockChain.trackTransations)
+app.listen(port, () => {
+console.log(`App running on port ${port}. API is pointed towards ${process.env.API}, with
+${process.env.WALLET} as the wallet account address.
+ ${process.env.FILE} is the ABI being used, on CHAINID ${process.env.CHAINID} WITH RPC ${process.env.RPC}`);
+});
